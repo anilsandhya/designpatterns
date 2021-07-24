b3dd4540313d25d3fee9bfbd19306b0db8168b78
asharma <anilsandhya@gmail.com>
2021-07-24 16:22:51 -0400

    Decorator Pattern

---
+++ javademo04 - Strategy Pattern/src/decoratorpattern01/Mocha.java
@@ -0,0 +1,15 @@
+package decoratorpattern01;
+
+public class Mocha extends CondimentDecorator {
+	public Mocha(Beverage beverage) {
+		this.beverage = beverage;
+	}
+	
+	public String getDescription() {
+		return beverage.getDescription() + ", Mocha";
+	}
+	
+	public double cost() {
+		return beverage.cost() + .20;
+	}
+}
