5623a7bbbd4803d20ff297650789acf10ce331c4
asharma <anilsandhya@gmail.com>
2021-07-25 12:31:28 -0400

    Factory Pattern

---
+++ javademo04 - Strategy Pattern/src/factorypattern01/NYStyleCheesePizza.java
@@ -0,0 +1,12 @@
+package factorypattern01;
+
+public class NYStyleCheesePizza extends Pizza {
+	
+	public NYStyleCheesePizza(){
+		name = "Ny Style Souce and Cheese Pizza";
+		dough = "Thin Crust Fough";
+		sauce = "Marinara Sauce";
+		
+		toppings.add("grated Regiano Cheese");
+	}
+}
---
+++ javademo04 - Strategy Pattern/src/factorypattern01/PizzaStore.java
@@ -0,0 +1,20 @@
+package factorypattern01;
+
+public abstract class PizzaStore {
+	
+	public Pizza OrderPizza(String type) {
+		Pizza pizza;
+		
+		pizza = createPizza(type);
+		
+		pizza.prepare();
+		pizza.bake();
+		pizza.cut();
+		pizza.box();
+		
+		return pizza;
+	}
+	
+	protected abstract Pizza createPizza(String type);
+
+}
